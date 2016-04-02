76ba5614fbfa799550b0fa877e0c89bf3d372fd7
Derek Guenther <dguenther9@gmail.com>
2016-04-01 22:38:18 -0700

    Fix leak of firstrun handler

--- firstrun.go
+++ firstrun.go
@@ -4,24 +4,33 @@ import (
 	"net/http"
 )
 
+func usersExist() bool {
+	c, err := getUserTable().Count().Run(dataStore.GetSession())
+	defer c.Close()
+	if err != nil {
+		return false
+	}
+	var count int
+	err = c.One(&count)
+	if err != nil {
+		return false
+	}
+	return count != 0
+}
+
 func saveFirstRunHandler(w http.ResponseWriter, r *http.Request) {
+	if usersExist() {
+		http.NotFound(w, r)
+		return
+	}
 	registerUser(r.FormValue("email"), r.FormValue("password"), getMaxPermissionLevel())
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func firstRunHandler(w http.ResponseWriter, r *http.Request) {
-	var count int
-	c, err := getUserTable().Count().Run(dataStore.GetSession())
-	defer c.Close()
-	if err != nil {
+	if usersExist() {
 		http.NotFound(w, r)
 		return
 	}
-	err = c.One(&count)
-	if err != nil || count != 0 {
-		http.NotFound(w, r)
-		return
-	}
-
 	renderTemplate(w, r, "firstRun", nil)
 }
