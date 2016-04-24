9b78254719a67e74b8c86f0d600ffb6dee88b52c
Derek Guenther <dguenther9@gmail.com>
2016-04-24 13:36:06 -0700

    Fix CORS for API requests

--- api.go
+++ api.go
@@ -5,7 +5,16 @@ import (
 	"net/http"
 )
 
-func writeAPIResponse(w http.ResponseWriter, data interface{}) {
+func writeCORSHeaders(w http.ResponseWriter, r *http.Request) {
+	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+	}
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+}
+
+func writeAPIResponse(w http.ResponseWriter, r *http.Request, data interface{}) {
 	buf := bufpool.Get()
 	defer bufpool.Put(buf)
 	if err := json.NewEncoder(buf).Encode(&data); err != nil {
@@ -13,12 +22,17 @@ func writeAPIResponse(w http.ResponseWriter, data interface{}) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	writeCORSHeaders(w, r)
 	buf.WriteTo(w)
+}
+
+func handleAPIPreflight(w http.ResponseWriter, r *http.Request) {
+	writeCORSHeaders(w, r)
 }
 
 func handleAPIPlayers(w http.ResponseWriter, r *http.Request) {
 	p := fetchPlayers()
-	writeAPIResponse(w, p)
+	writeAPIResponse(w, r, p)
 }
 
 func handleAPIPlayersSearch(w http.ResponseWriter, r *http.Request) {
@@ -28,5 +42,5 @@ func handleAPIPlayersSearch(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	writeAPIResponse(w, p)
+	writeAPIResponse(w, r, p)
 }
